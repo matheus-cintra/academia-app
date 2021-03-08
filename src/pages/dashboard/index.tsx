1fe7e66584a9abb1e3f2fbe022e4f64b8b96034a
Matheus Cintra <matheus_cintra@hotmail.com>
2021-03-08 17:43:34 -0300

    craete simples dashbaord

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles(theme => {
       height: 120,
       justifyContent: 'center',
       alignItems: 'center',
+      marginBottom: 24,
     },
     bullet: {
       display: 'inline-block',
